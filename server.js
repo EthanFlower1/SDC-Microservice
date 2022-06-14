4e6dd2d88c6d10c390363398744c237cd66a14ce
Ethan Flower <ethanjamesflower@gmail.com>
2022-06-13 22:39:19 -0500

    add loader file

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const reviewRouter = require('./routes');
 require('dotenv').config();
 
+app.use('/loaderio-c61a5a16f43554d40c649ca3a82facc7.txt', (req, res) => {
+  res.send("./loaderio-c61a5a16f43554d40c649ca3a82facc7.txt")
+})
 app.use('/reviews', reviewRouter)
 
 app.listen(port, () =>
